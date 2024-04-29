2b4110b2a442dfafdc20be909ec48f530a9217b7
Utsab Haldar <utsabhaldar88888@gmail.com>
2024-04-29 10:13:06 +0530

    Home page made

---
+++ lib/Pages/home_page.dart
@@ -0,0 +1,90 @@
+import 'package:currency_converter/Constants/colors.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        width: 2,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.circular(60),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: cardColor,
+        elevation: 0,
+        title: const Text(
+          'Currency Converter',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: const Color(0xFF343541),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              '0',
+              style: TextStyle(
+                fontSize: 55,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Amount in USD',
+                  hintStyle: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  prefixIcon: const Icon(Icons.attach_money_rounded),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextButton(
+                onPressed: () {
+                  if (kDebugMode) print('button');
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: cardColor,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                child: const Text('Convert'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
